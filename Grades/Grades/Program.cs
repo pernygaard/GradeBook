e3fe26b1b4f8dda72cc7d1ca365435a843a4dc8a
Per Nygaard <pernygaard.online@gmail.com>
2015-07-30 08:56:34 +0200

    Initial files

---
+++ Grades/Grades/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(91f);
+            book.AddGrade(89.1f);
+
+            GradeBook book2 = book;
+            book2.AddGrade(75);
+        }
+    }
+}
