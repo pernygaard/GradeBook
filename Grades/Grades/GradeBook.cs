622c4dbdd63bd5fb929c50a111f53ca5f5096b0f
Per Nygaard <pernygaard.online@gmail.com>
2015-07-30 08:55:52 +0200

    initial files

---
+++ Grades/Grades/GradeBook.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Grades
+{
+    class GradeBook
+    {
+        public GradeBook()
+        {
+            grades = new List<float>();
+        }
+        public void AddGrade(float grade)
+        {
+            grades.Add(grade);
+        }
+
+        List<float> grades;
+    }
+}
